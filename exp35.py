949a0334847dbd5e6ceedef1c2356ca8371d5376
Nishanth2480 <nishanth.direct@gmail.com>
2025-07-25 11:01:56 +0530

    Update exp35.py

--- exp35.py
+++ exp35.py
@@ -2,16 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import os
 
 service = Service("C:\\WebDriver\\chromedriver-win64\\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+action = ActionChains(driver)
+
 driver.maximize_window()
 driver.get("http://primevideo.com/")
 time.sleep(2)
-search = driver.find_element(By.XPATH , "/html/body/div/div[1]/div[2]/div/header/nav/div[1]/div/div[2]/ul/li[1]/div/button/span").click()
+search = driver.find_element(By.XPATH , "/html/body/div/div[1]/div[2]/div/header/nav/div[1]/div/div[2]/ul/li[1]/div/button/span")
+action.move_to_element(search).click().perform()
 time.sleep(5)
 search_bar = driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[2]/div/header/nav/div[1]/div/div[2]/ul/li[1]/div[1]/div/div/div/div/form/input[2]")
 search_bar.send_keys("Master")
